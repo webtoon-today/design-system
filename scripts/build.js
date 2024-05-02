db752dd6688e96024287b8fa4d1d407be8705b67
youngmin <youngmin.kim@webtoontoday.com>
2024-05-02 17:12:35 +0900

    chore: use package.json, component folder name PascalCase

--- scripts/build.js
+++ scripts/build.js
@@ -5,11 +5,21 @@ const path = require('path');
 const child_process = require('child_process')
 
 const packageDirectories = fs.readdirSync(path.resolve(__dirname, '../packages'));
-const componentDirectories = packageDirectories
-    .map((directory) => fs.readdirSync(path.resolve(__dirname, `../packages/${directory}`)))
+const packageNames = packageDirectories
+    .map((directory) => {
+        const componentDirectories = fs.readdirSync(path.resolve(__dirname, `../packages/${directory}`));
+
+        return componentDirectories.map((component) => {
+            const packageJsonPath = path.resolve(__dirname, `../packages/${directory}/${component}/package.json`);
+            const packageName = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8')).name;
+            return packageName;
+        });
+    })
     .flat();
 
-componentDirectories.forEach((packageName) => {
+console.log(packageNames)
+
+packageNames.forEach((packageName) => {
     const buildCommand = `npm run build -w ${packageName}`;
     console.log(buildCommand);
     child_process.execSync(buildCommand, {stdio: 'inherit'});
