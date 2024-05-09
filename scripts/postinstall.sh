4e7c8e8c29e149c2da36c8af2db3642f18ecaeb1
youngmin <youngmin.kim@webtoontoday.com>
2024-05-09 09:49:47 +0900

    fix: typo sh script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,10 @@
-if [ -d "./.github" ] then  
-    echo "skip postinstall script" >2
-    exit 0 
+if [ -d "./.github" ]; then
+  echo "skip postinstall script"
+  exit 0
 fi  
 
-if [ -d "./dist" ] then  
-    cp -r ./dist/* ./
-    rm -rf ./dist
-    echo "postinstall script executed successfully" >2  
+if [ -d "./dist" ]; then  
+  cp -r ./dist/* ./
+  rm -rf ./dist
+  echo "postinstall script executed successfully"
 fi
