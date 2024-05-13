2bd5e9f423418f7ab6d3f6b20c921d7102d5c16a
youngmin <youngmin.kim@webtoontoday.com>
2024-05-13 10:11:11 +0900

    feat: moremenu interaction story

--- packages/navigation/MoreMenu/src/MoreMenu.stories.tsx
+++ packages/navigation/MoreMenu/src/MoreMenu.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import {userEvent, within} from '@storybook/test';
 import React from 'react';
 import MoreMenu from './MoreMenu';
 
@@ -8,34 +9,72 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
+  args: {
+    children: '',
+    ButtonElementChild: '',
+    className: '',
+    PaperProps: undefined,
+    anchorOrigin: {
+        vertical: 'bottom',
+        horizontal: 'right',
+    },
+    transformOrigin: {
+        vertical: 'top',
+        horizontal: 'left',
+    },
+  },
   tags: ['autodocs']
 } satisfies Meta<typeof MoreMenu>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default = () => {
+const defaultArgs: Story['args'] = {
+  children: '',
+  ButtonElementChild: '',
+  className: '',
+  PaperProps: undefined,
+  anchorOrigin: {
+      vertical: 'bottom',
+      horizontal: 'right',
+  },
+  transformOrigin: {
+      vertical: 'top',
+      horizontal: 'left',
+  },
+}
+
+const DefaultTemplete = (args:Story['args']) => {
     return (
-        <div>
-            <MoreMenu
-                ButtonElementChild={
-                    <img src='https://static.webtoon.today/ddah/icon/more_horiz.svg' style={{transform: 'rotate(90deg)'}} alt='more_horiz' width={20} height={20} />
-                }
-                anchorOrigin={{
-                    horizontal: 'right',
-                }}
-                transformOrigin={{
-                    horizontal: 'right',
-                }}
-            >
-                <MoreMenu.MenuItemButton 
-                    onClick={ () => { 
-                        alert('click');
-                    }}
-                >
-                    {'click'}
-                </MoreMenu.MenuItemButton>
-            </MoreMenu>
-        </div>
-    )
+      <div>
+        <MoreMenu
+          ButtonElementChild={
+              args?.ButtonElementChild ||
+              <img src='https://static.webtoon.today/ddah/icon/more_horiz.svg' style={{transform: 'rotate(90deg)'}} alt='more_horiz' width={20} height={20} />
+          }
+          anchorOrigin={args?.anchorOrigin}
+          transformOrigin={args?.transformOrigin}
+        >
+          <MoreMenu.MenuItemButton onClick={ () => alert('click')}>
+              {'click'}
+          </MoreMenu.MenuItemButton>
+        </MoreMenu>
+      </div>
+  )
+}
+
+export const Default:Story = {
+  args: defaultArgs,
+  render: DefaultTemplete
+}
+
+export const Interaction:Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const button = canvas.getByRole('button');
+    await userEvent.click(button);
+  },
+  args: defaultArgs,
+  render: DefaultTemplete
 }
