32be1f4b3852c89efbdbcc55f3b44759bbc94ed0
youngmin <youngmin.kim@webtoontoday.com>
2024-05-14 09:32:16 +0900

    feat: docs react app

--- scripts/deploy_storybook.sh
+++ scripts/deploy_storybook.sh
@@ -1,4 +1,5 @@
-aws s3 sync s3://front-design-system.webtoon.today --acl public-read
+GIT_BRANCH="$1"
 
-# TODO: update cloudfront id
-aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths "/*" | cat
+aws s3 sync storybook-static s3://front-design-system.webtoon.today/${GIT_BRANCH} --acl public-read
+
+aws cloudfront create-invalidation --distribution-id E2U4FV88FXPRPU --paths "/${GIT_BRANCH}/*" | cat
