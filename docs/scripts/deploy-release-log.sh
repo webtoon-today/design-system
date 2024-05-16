1bc54caa5d2475e43fa81928a70cb3d942b76256
youngmin <youngmin.kim@webtoontoday.com>
2024-05-16 12:12:59 +0900

    chore: release log script

--- docs/scripts/deploy-release-log.sh
+++ docs/scripts/deploy-release-log.sh
@@ -1,3 +1,5 @@
-aws s3 sync docs/public/RELEASE_LOG.md s3://front-design-system.webtoon.today --acl public-read
+cat public/RELEASE_LOG.md
 
-aws cloudfront create-invalidation --distribution-id E2U4FV88FXPRPU --paths "/*" | cat
+aws s3 sync public/RELEASE_LOG.md s3://front-design-system.webtoon.today --acl public-read
+
+aws cloudfront create-invalidation --distribution-id E2U4FV88FXPRPU --paths "/RELEASE_LOG.md" | cat
