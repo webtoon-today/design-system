f962093f9e7ea369a89fac46574c5cb69a437212
youngmin <youngmin.kim@webtoontoday.com>
2024-05-17 12:12:44 +0900

    feat: display update time

--- docs/src/Component/ReleaseNote.js
+++ docs/src/Component/ReleaseNote.js
@@ -28,33 +28,40 @@ const ReleaseNote = () => {
         <div className={'ReleaseNote'}>
             <h1>{'Release Note'}</h1>
             <List>
-                {releaseNoteList.map((releaseNote) => (
-                    <ListItem key={releaseNote.githubUrl}>
-                        <Card className={'ReleaseNoteCard'}>
-                            <details>
-                                <summary>
-                                    <h1 className={'ReleaseTitle'}>
-                                        <a href={releaseNote.githubUrl}>
-                                            {releaseNote.title}
-                                        </a>
-                                    </h1>
-                                    <KeyboardArrowUp className={'Arrow'}/>
-                                </summary>
-                                <div className={'ReleaseBody'}>
-                                    <ReactMarkdown
-                                        children={releaseNote.body}
-                                        components={{
-                                            h1: `h2`,
-                                            h2: `h3`,
-                                            h3: `h4`,
-                                        }}
-                                    />
-                                </div>
-                                <a href={`/${releaseNote.branch}`}>{'storybook preview'}</a>
-                            </details>
-                        </Card>
-                    </ListItem>
-                ))}
+                {releaseNoteList.map((releaseNote) => {
+                    const updateDate = new Date(releaseNote.updatedAt);
+                    const updateDateString = `${updateDate.getFullYear()}-${updateDate.getMonth() + 1}-${updateDate.getDate()} ${updateDate.getHours()}:${updateDate.getMinutes()}`;
+                    return (
+                        <ListItem key={releaseNote.githubUrl}>
+                            <Card className={'ReleaseNoteCard'}>
+                                <details>
+                                    <summary>
+                                        <div className={'ReleaseTitle'}>
+                                            <h1 >
+                                                <a href={releaseNote.githubUrl}>
+                                                    {releaseNote.title}
+                                                </a>
+                                            </h1>
+                                            <span>{`update time: ${updateDateString}`}</span>
+                                        </div>
+                                        <KeyboardArrowUp className={'Arrow'}/>
+                                    </summary>
+                                    <div className={'ReleaseBody'}>
+                                        <ReactMarkdown
+                                            children={releaseNote.body}
+                                            components={{
+                                                h1: `h2`,
+                                                h2: `h3`,
+                                                h3: `h4`,
+                                            }}
+                                        />
+                                    </div>
+                                    <a href={`/${releaseNote.branch}`}>{'storybook preview'}</a>
+                                </details>
+                            </Card>
+                        </ListItem>
+                    )
+                })}
             </List>
         </div>
     )
