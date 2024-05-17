a50a1ab22bf2e886582a63c407f4d5de1e41f01f
youngmin <youngmin.kim@webtoontoday.com>
2024-05-17 09:45:19 +0900

    chore: build manager head

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,37 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../packages/**/**/src/*.mdx", "../packages/**/**/src/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "@storybook/preset-scss",
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+    stories: ["../packages/**/**/src/*.mdx", "../packages/**/**/src/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-webpack5-compiler-swc",
+        "@storybook/addon-onboarding",
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@chromatic-com/storybook",
+        "@storybook/addon-interactions",
+        "@storybook/preset-scss",
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    managerHead: (head) => `
+    ${head}
+    <script>
+        window.onload=function () {
+            if (window.location.pathname === "/") {
+                return;
+            }
+            let session=localStorage.getItem("session");
+            if (!session || !JSON.parse(session)?.userid) {
+                window.location.href = "/"; 
+            }
+        }
+    </script>
+    `
 };
 export default config;
+
--- scripts/build_storybook.sh
+++
@@ -1,6 +0,0 @@
-storybook build
-
-INDEX=$(cat storybook-static/index.html)
-
-echo $INDEX | sed 's/<\/head\>/\<script\>window.onload=function(){if(window.location.pathname === "\/"){return;}let session=localStorage.getItem(\"session\");if (!session || !JSON.parse(session)?.userid) { window.location.href = "\/"; return;}}\<\/script\>\<\/head\>/g' > storybook-static/temp.html
-mv storybook-static/temp.html storybook-static/index.html
