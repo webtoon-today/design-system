b2b2a84379df309930bf9ab8f2057add5fc9e92d
youngmin <youngmin.kim@webtoontoday.com>
2024-05-17 14:16:23 +0900

    fix: deploy-storybook exception handling

--- scripts/deploy_storybook.sh
+++ scripts/deploy_storybook.sh
@@ -1,5 +1,10 @@
 CURRENT_BRANCH=$(git branch --show-current)
 
+if [ -z "$CURRENT_BRANCH" ]; then
+    echo "current branch is not found"
+    exit 1
+fi
+
 aws s3 sync storybook-static s3://front-design-system.webtoon.today/${CURRENT_BRANCH} --acl public-read
 
 aws cloudfront create-invalidation --distribution-id E2U4FV88FXPRPU --paths "/${CURRENT_BRANCH}/*" | cat
