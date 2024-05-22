80ced81c21735036789e33894d03c19845208ec6
youngmin <youngmin.kim@webtoontoday.com>
2024-05-22 11:59:59 +0900

    feat: test page toast, drawer

--- packages/page/TestPage/src/TestPage.tsx
+++ packages/page/TestPage/src/TestPage.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+
+import { useToastAlert } from 'Toast';
+import { PortalDrawer } from 'Drawer';
 
 const TestPage = () => {
 
+    const { toastAlert } = useToastAlert();
+
+    const [open, setOpen] = useState(false);
+
+    const container = useRef<HTMLDivElement>(null);
+
     return (
-        <div>
+        <div ref={container}>
             <h1>Test Page</h1>
+
+            <div>
+                <h2>button group</h2>
+                <button onClick={()=>toastAlert('hello test page')}>toast</button>
+                <button onClick={()=>setOpen(true)}>open drawer</button>
+            </div>
+            <PortalDrawer isClient open={open} onClose={()=>setOpen(false)} container={container.current}>
+                <div>
+                    <h2>hello portal drawer</h2>
+                    <button onClick={()=>setOpen(false)}>close</button>
+                </div>
+            </PortalDrawer>
         </div>
     )
 }
