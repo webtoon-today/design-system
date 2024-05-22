b71f5ddebce22f98c952bae672a32e58b748491f
youngmin <youngmin.kim@webtoontoday.com>
2024-05-22 16:38:25 +0900

    chore: reduce addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,4 @@
-import type { AddonOptionsWebpack } from '@storybook/addon-coverage';
 import type { StorybookConfig } from "@storybook/react-webpack5";
-
-const coverageOptions:AddonOptionsWebpack = {
-    istanbul: {
-        include: ['../packages/**/**/src/*.stories.@(ts|tsx)'],
-        extension: ['.ts', '.tsx'],
-    }
-}
 
 const config: StorybookConfig = {
     stories: ["../packages/**/**/src/*.mdx", "../packages/**/**/src/*.stories.@(ts|tsx)"],
@@ -17,10 +9,7 @@ const config: StorybookConfig = {
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
         "@storybook/preset-scss",
-        {
-          name: '@storybook/addon-coverage',
-          options: coverageOptions,
-        }
+        "@storybook/addon-coverage"
     ],
     framework: {
         name: "@storybook/react-webpack5",
