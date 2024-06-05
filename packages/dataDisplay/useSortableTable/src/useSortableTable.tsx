d276aa937cf5ee0fc10507124715da4a944f4433
sugeul <sugeul.jin@gmail.com>
2024-06-05 22:46:11 +0900

    [sorted table] 데이터가 변경되면 테이블을 다시 정렬

--- packages/dataDisplay/useSortableTable/src/useSortableTable.tsx
+++ packages/dataDisplay/useSortableTable/src/useSortableTable.tsx
@@ -2,6 +2,8 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 type SortableTableDataType<V extends Object> = Map<keyof V, Array<V[keyof V]>>;
 
+const unique = <T,>(val: T, idx: number, arr: T[]) => arr.indexOf(val) === idx;
+
 const _initalizeConvertedData = <V extends Object,>(data: V[], keys: (keyof V)[]) => {
     const initTableData: SortableTableDataType<V> = new Map();
 
@@ -28,40 +30,60 @@ const useSortableTable = <V extends Object>(data: V[]) => {
 
     const [convertedData, setConvertedData] = useState<SortableTableDataType<V> | undefined>(undefined);
     const [sortedData, setSortedData] = useState<V[]>(() => data);
+    const [key, setKey] = useState<K>();
+    const [compareFn, setCompareFn] = useState<(a: any, b: any) => number>();
 
     useEffect(() => {
         if (data.length === 0) {
             return;
         }
-        setSortedData(data);
+        const keys = data.map(row => Object.keys(row) as K[]).flat().filter(unique);
+
         setConvertedData(_initalizeConvertedData(data, keys));
     }, [data]);
-
-    const keys = useMemo(() => {
-        const objectKeys = data.reduce((lhs, rhs) => {
-            const currentValueKeys = Object.keys(rhs) as K[];
-
-            const newObjectKeys = currentValueKeys.filter((key) => !lhs.includes(key));
-
-            return [...lhs, ...newObjectKeys];
-        }, [] as K[]);
-
-        return objectKeys;
-    }, [data]);
     
+    /** validate params and pass key, compareFn */
     const sort = useCallback((key: K, compareFn: (a: any, b: any) => number) => {
-        if (keys.length === 0) {
-            return;
-        }
-
         if (convertedData === undefined) {
             throw new Error('tableData is not initialized');
+        }
+
+        if (!key || !compareFn){
+            throw new Error('sort parameters are not given');
+        }
+
+        const keys = [...convertedData.keys()];
+
+        if (keys.length === 0) {
+            return;
         }
 
         const targetColumn = convertedData.get(key);
 
         if (targetColumn === undefined) {
             throw new Error('Can not find key in data');
+        }
+
+        setKey(key);
+        setCompareFn(()=>compareFn);
+
+    }, [convertedData]);
+
+    /** sort data depends on convertedData, key, compareFn */
+    useEffect(()=>{
+        if (!convertedData || !key || !compareFn){
+            return;
+        }
+        
+        const keys = [...convertedData.keys()];
+        
+        const targetColumn = convertedData.get(key);
+
+        if (targetColumn === undefined) {
+            setKey(undefined);
+            setCompareFn(undefined);
+            console.error('Can not find key in data');
+            return;
         }
 
         const sortedIndex = targetColumn
@@ -71,28 +93,26 @@ const useSortableTable = <V extends Object>(data: V[]) => {
 
         const newSortedData = sortedIndex.map((index) =>
             keys.map((key) => {
-                const value = convertedData.get(key);
-
-                if (!value) {
-                    throw new Error('Can not find key in data');
-                }
-
-                return { [key]: value[index] };
+                return { [key]: targetColumn[index] };
             }).reduce((lhs, rhs) => {
                 return {...lhs, ...rhs}; 
             }, {} as V)
         );
-
-            setSortedData(newSortedData);
-        }, [convertedData, keys]);
+        
+        setSortedData(newSortedData);
+    
+    },[convertedData, key, compareFn])
 
     const initializeSort = useCallback(() => {
+
+        const keys = data.map(row => Object.keys(row) as K[]).flat().filter(unique);
+
         if (keys.length === 0) {
             return;
         }
         setConvertedData(_initalizeConvertedData(data, keys));
         setSortedData(data);
-    }, [data, keys]);
+    }, [data]);
 
     const sortableTable = {
         sort,
