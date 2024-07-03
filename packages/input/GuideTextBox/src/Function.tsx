369f50762dc18b6f52c0775a724f081ae134e3af
DY414 <tmvldghkfur@gmail.com>
2024-07-03 13:57:13 +0900

    - fix order of conditioin

--- packages/input/GuideTextBox/src/Function.tsx
+++ packages/input/GuideTextBox/src/Function.tsx
@@ -5,13 +5,13 @@ import { buttonStatusType, guideTextType } from "./Type";
 export const capitalizeFirstLetter = (str: string) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 
 export const getGuideTextType = (conditions: {[key in guideTextType]?: boolean}) => {
-    const foundType = ([ "fail", "success", "required" ] as const).find( type => conditions[type] )
+    const foundType = ([ "required", "success", "fail"  ] as const).find( type => conditions[type] )
     
     return foundType ? foundType : 'normal';
 }
 
 export const getButtonStatusType = (conditions: {[key in buttonStatusType]?: boolean}) => {
-    const foundType = (['inactivated', 'fail', 'success', 'pending'] as const).find( type => conditions[type])
+    const foundType = (['inactivated', 'pending', 'success', 'fail' ] as const).find( type => conditions[type])
     
     return  foundType ? foundType : 'activated';
 }
